8c4a41da87efd1b3e8b5ebb27f922380976819ce
AmandaAzania <mandygudlwa@gmail.com>
2022-09-08 13:22:32 +0200

    headers

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.use(express.static('views'))
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
 });
 
